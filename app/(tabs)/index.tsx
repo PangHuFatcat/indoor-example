85b1bce60e4c13d55b9a3ec1273c0f9804372e90
周广俊 <743137213@qq.com>
2024-05-18 14:46:51 +0800

    feat 添加google maps

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,11 +4,13 @@ import { HelloWave } from "@/components/HelloWave"
 import ParallaxScrollView from "@/components/ParallaxScrollView"
 import { ThemedText } from "@/components/ThemedText"
 import { ThemedView } from "@/components/ThemedView"
+import MapView from "react-native-maps"
 
 export default function HomeScreen() {
   return (
     <ThemedView style={{ flex: 1 }}>
-      <ThemedView style={{ flex: 1 }}></ThemedView>
+      <MapView style={{ flex: 1 }}></MapView>
+      {/* <ThemedView style={{ flex: 1 }}></ThemedView> */}
     </ThemedView>
   )
 }
